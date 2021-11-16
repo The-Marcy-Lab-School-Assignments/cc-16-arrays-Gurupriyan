eda4e36370f122c55ea14b4db2b45030c7f9555f
Gurupriyan <gurupriyan98@gmail.com>
2021-11-16 15:41:01 +0000

    Completed the code challenge

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 //write your code here
+function replaceWithYerr(arr, startIndex = 0, deleteCount = arr.length) {
+    let result = arr.fill("yerr", startIndex, deleteCount+1);
+    return result;
+}
+
+console.log(replaceWithYerr(['Jan', 'February', 'March', 'April', 'May', 'June']));
