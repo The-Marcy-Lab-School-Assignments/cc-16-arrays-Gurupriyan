d016c4eb517b15671bdf5de2ffe42d504cb8ecf8
Gurupriyan <gurupriyan98@gmail.com>
2021-11-16 15:51:12 +0000

    Changed code to not mutate the org array

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //write your code here
 function replaceWithYerr(arr, startIndex = 0, deleteCount = arr.length) {
-    let result = arr.fill("yerr", startIndex, deleteCount+1);
+    let result = [...arr]
+    result = result.fill("yerr", startIndex, deleteCount+1);
     return result;
 }
 
-console.log(replaceWithYerr(['Jan', 'February', 'March', 'April', 'May', 'June']));
+
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+console.log(replaceWithYerr(months));
+console.log(months)
